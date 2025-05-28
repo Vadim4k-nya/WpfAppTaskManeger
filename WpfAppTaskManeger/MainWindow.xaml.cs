b496f85ff39f4c5b77e1c2a61796ac81e429dcc7
dasadno <Yakaleks96@gmail.com>
2025-05-28 10:44:45 +0500

    done

--- WpfAppTaskManeger/MainWindow.xaml.cs
+++ WpfAppTaskManeger/MainWindow.xaml.cs
@@ -60,7 +60,7 @@ namespace WpfAppTaskManeger
                 {
                     toDoList.Remove(taskForDelete);
 
-                    UpdateListToDo();
+                    UpdateListToDo(); 
                 }
             }
             else
