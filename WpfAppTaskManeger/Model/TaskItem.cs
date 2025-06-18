25e2476a8d5826d1f418347f07f423e4fcc44f7c
dasadno <Yakaleks96@gmail.com>
2025-06-18 10:13:08 +0500

    clean

--- WpfAppTaskManeger/Model/TaskItem.cs
+++ WpfAppTaskManeger/Model/TaskItem.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Runtime.InteropServices.JavaScript.JSType;
-
-namespace WpfAppTaskManeger.Model
+﻿namespace WpfAppTaskManeger.Model
 {
     /// <summary>
     /// Отдельный элемент задачи в списке дел
